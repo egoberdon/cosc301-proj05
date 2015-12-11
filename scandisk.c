b3ef88a09704e3a81ff8f4bdb59613b7993d269b
egoberdon <goberdon@gmail.com>
2015-12-10 20:21:21 -0500

    copied dos_ls functions into main and added clust_ref array

--- scandisk.c
+++ scandisk.c
@@ -14,6 +14,139 @@
 #include "fat.h"
 #include "dos.h"
 
+uint16_t print_dirent(struct direntry *dirent, int indent, int* clust_ref)
+{
+    uint16_t followclust = 0;
+
+    int i;
+    char name[9];
+    char extension[4];
+    uint32_t size;
+    uint16_t file_cluster;
+    name[8] = ' ';
+    extension[3] = ' ';
+    memcpy(name, &(dirent->deName[0]), 8);
+    memcpy(extension, dirent->deExtension, 3);
+    if (name[0] == SLOT_EMPTY)
+    {
+	return followclust;
+    }
+
+    /* skip over deleted entries */
+    if (((uint8_t)name[0]) == SLOT_DELETED)
+    {
+	return followclust;
+    }
+
+    if (((uint8_t)name[0]) == 0x2E)
+    {
+	// dot entry ("." or "..")
+	// skip it
+        return followclust;
+    }
+
+    /* names are space padded - remove the spaces */
+    for (i = 8; i > 0; i--)
+    {
+	if (name[i] == ' ')
+	    name[i] = '\0';
+	else
+	    break;
+    }
+
+    /* remove the spaces from extensions */
+    for (i = 3; i > 0; i--)
+    {
+	if (extension[i] == ' ')
+	    extension[i] = '\0';
+	else
+	    break;
+    }
+
+    if ((dirent->deAttributes & ATTR_WIN95LFN) == ATTR_WIN95LFN)
+    {
+	// ignore any long file name extension entries
+	//
+	// printf("Win95 long-filename entry seq 0x%0x\n", dirent->deName[0]);
+    }
+    else if ((dirent->deAttributes & ATTR_VOLUME) != 0)
+    {
+	printf("Volume: %s\n", name);
+    }
+    else if ((dirent->deAttributes & ATTR_DIRECTORY) != 0)
+    {
+        // don't deal with hidden directories; MacOS makes these
+        // for trash directories and such; just ignore them.
+	if ((dirent->deAttributes & ATTR_HIDDEN) != ATTR_HIDDEN)
+        {
+    	    printf("%s/ (directory)\n", name);
+            file_cluster = getushort(dirent->deStartCluster);
+            followclust = file_cluster;
+        }
+    }
+    else
+    {
+        /*
+         * a "regular" file entry
+         * print attributes, size, starting cluster, etc.
+         */
+	int ro = (dirent->deAttributes & ATTR_READONLY) == ATTR_READONLY;
+	int hidden = (dirent->deAttributes & ATTR_HIDDEN) == ATTR_HIDDEN;
+	int sys = (dirent->deAttributes & ATTR_SYSTEM) == ATTR_SYSTEM;
+	int arch = (dirent->deAttributes & ATTR_ARCHIVE) == ATTR_ARCHIVE;
+
+	size = getulong(dirent->deFileSize);
+	printf("%s.%s (%u bytes) (starting cluster %d) %c%c%c%c\n",
+	       name, extension, size, getushort(dirent->deStartCluster),
+	       ro?'r':' ',
+               hidden?'h':' ',
+               sys?'s':' ',
+               arch?'a':' ');
+    }
+
+    return followclust;
+}
+
+
+void follow_dir(uint16_t cluster, int indent,
+		uint8_t *image_buf, struct bpb33* bpb, int* clust_ref)
+{
+    while (is_valid_cluster(cluster, bpb))
+    {
+        struct direntry *dirent = (struct direntry*)cluster_to_addr(cluster, image_buf, bpb);
+
+        int numDirEntries = (bpb->bpbBytesPerSec * bpb->bpbSecPerClust) / sizeof(struct direntry);
+        int i = 0;
+	for ( ; i < numDirEntries; i++)
+	{
+
+            uint16_t followclust = print_dirent(dirent, indent, clust_ref);
+            if (followclust)
+                follow_dir(followclust, indent+1, image_buf, bpb, clust_ref);
+            dirent++;
+	}
+
+	cluster = get_fat_entry(cluster, image_buf, bpb);
+    }
+}
+
+
+void traverse_root(uint8_t *image_buf, struct bpb33* bpb, int* clust_ref)
+{
+    uint16_t cluster = 0;
+
+    struct direntry *dirent = (struct direntry*)cluster_to_addr(cluster, image_buf, bpb);
+
+    int i = 0;
+    for ( ; i < bpb->bpbRootDirEnts; i++)
+    {
+        uint16_t followclust = print_dirent(dirent, 0, clust_ref);
+        if (is_valid_cluster(followclust, bpb))
+            follow_dir(followclust, 1, image_buf, bpb, clust_ref);
+
+        dirent++;
+    }
+}
 
 void usage(char *progname) {
     fprintf(stderr, "usage: %s <imagename>\n", progname);
@@ -33,11 +166,11 @@ int main(int argc, char** argv) {
     bpb = check_bootsector(image_buf);
 
     // your code should start here...
-
-
-
-
-
+    int clust_ref[2880];
+    for(int i = 0; i < 2880; i++){
+      clust_ref[i] = 0;
+    }
+    traverse_root(image_buf, bpb, clust_ref);
 
     unmmap_file(image_buf, &fd);
     return 0;
